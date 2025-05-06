2be6faca9cc29f65cb91f794a07fe5fd7d8133fc
Johannes Brandt <jbrandt@2-F-3.42heilbronn.de>
2025-05-06 17:21:43 +0200

    updated makefile so it removes the mlx42 when you do fclean

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,14 +6,14 @@
 /*   By: jbrandt <jbrandt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 16:27:29 by jbrandt           #+#    #+#             */
-/*   Updated: 2025/05/06 16:36:52 by jbrandt          ###   ########.fr       */
+/*   Updated: 2025/05/06 17:21:36 by jbrandt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../lib/MLX42/include/MLX42/MLX42.h"
+# include "../MLX42/include/MLX42/MLX42.h"
 # include "../libft/include/libft.h"
 # include <math.h>
 # include <stdio.h>
